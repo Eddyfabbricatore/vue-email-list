5f3b8e795e88b2f7d2a7f0ecb275833c9c1612d7
Eddyfabbricatore <eddyfabbricatore2@gmail.com>
2023-10-09 15:39:11 +0200

    [add] getApi() [add] getMails()

---
+++ script.js
@@ -0,0 +1,37 @@
+/*
+- Data la seguente API https://flynn.boolean.careers/exercises/api/random/mail
+- Generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
+- Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+*/
+
+const {createApp} = Vue;
+
+createApp({
+  data(){
+    return{
+      titolo: 'Elenco email',
+      apiUrl: 'https://flynn.boolean.careers/exercises/api/random/mail',
+      mailsList: [],
+      limit: 10
+    }
+  },
+
+  methods:{
+    getApi(){
+      axios.get(this.apiUrl)
+      .then((result) => {
+        this.mailsList.push(result.data.response);
+      })
+    },
+
+    getMails(){
+      for(let i = 1; i <= this.limit; i++){
+        this.getApi();
+      }
+    }
+  },
+
+  mounted(){
+    setTimeout(this.getMails(), 1000);
+  }
+}).mount('#app');
